6d18021d047126c8ec1ffc0bafba48eb3c5dafdd
Arthur Henrique <39110264+thurcst@users.noreply.github.com>
2020-10-29 16:59:59 -0300

    Update modificaaqui.py

--- modificaaqui.py
+++ modificaaqui.py
@@ -1,7 +1,7 @@
 def house_model(y_new):
 
-    xs =  np.array( [ -1,0,1,2,3,4 ] , dtype = float)
-    ys =  np.array( [ -1,0,1,2,3,4 ] , dtype = float)
+    xs =  np.array( [ -1,0,1,2,3,4 ] , dtype = float)   # Numpy array generator, as float
+    ys =  np.array( [ -1,0,1,2,3,4 ] , dtype = float)   # Numpy array generator, as float
     model = tf.keras.Sequential( [keras.layers.Dense( units = 1, input_shape = [1] )])
     model.compile(optimizer = "sgd" ,  loss = ' mean_squared_error')
     model.fit(xs,ys, epochs = 500)
